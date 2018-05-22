ea16e12cdb421d3d2c58b2e23d55f4e0d1b537a7
IsmaelCesar <leamscesar@gmail.com>
2018-05-22 00:33:17 -0300

    Busca em ordem
    Parcialmente concluida

--- avl_mips_assembly/MyAvl.asm
+++ avl_mips_assembly/MyAvl.asm
@@ -1,17 +1,17 @@
 .data
 
 #Estruturas ============================================================
-#Node: .space 28 #Este ï¿½ o espaï¿½o da estrutura nï¿½
+#Node: .space 28 #Este é o espaço da estrutura nó
 memory: .space 20000
-TreeRoot: .space 4      #Este ï¿½ o espaï¿½o onde serï¿½ guardado O endereco de memoria da raiz
-valAlocation: .space 4  #alocndo espaï¿½o para deslocamento de alocacao
-valDecremento: .space 4 #O espaï¿½o onde estï¿½ gravado o valor do decremento 
+TreeRoot: .space 4      #Este é o espaço onde será guardado O endereco de memoria da raiz
+valAlocation: .space 4  #alocndo espaço para deslocamento de alocacao
+valDecremento: .space 4 #O espaço onde está gravado o valor do decremento 
 bufferValor: .space 4 #alocando espaco para o valor
 #Arquivos ==============================================================
 buffer: .space 8
 espaco: .asciiz " "
-fin: .asciiz "tree.in"
-fout: .asciiz "tree.out"
+fin: .asciiz "../AvlAssembly/tree.in"
+fout: .asciiz "../AvlAssembly/tree.out"
 .text
 	#Abrindo Arquivo
 	li $v0, 13
@@ -39,7 +39,7 @@ main:
 	
 	li $a0, 0    #Zeraando $a0
 	li $v0, 42
-	li $a1, 501 #Gerando numeros aleatï¿½rios entre 0 e 500
+	li $a1, 501 #Gerando numeros aleatórios entre 0 e 500
 	syscall
 	
 	#Carregando no buffer
@@ -61,7 +61,8 @@ inserindoValores:
 	li $t8, 4 #aquiFoiModifica
 	addi $a0, $t8, 0
 	li $v0, 42
-	li $a1, 1001 #Gerando numeros aleatï¿½rios entre 0 e 1000	li $a1, 10000 #Gerando numeros aleatï¿½rios entre 0 e 1000	syscall 
+	li $a1, 9999 #Gerando numeros aleatórios entre 0 e 10000
+	syscall 
 	
 	addi $t8, $zero, 0    #Zerando $t8 para uso posterior
 	
@@ -107,7 +108,7 @@ buscar: addi $sp, $sp, -4
 	addi $t3, $a0, 0 #Salvando o valor buscado em $t3
 	addi $t1, $a1, 0 #salvando o endereco do node Raiz em $t1
 	
-	beq $t0, 0, endLoopBusca #Se o nï¿½ for nulo
+	beq $t0, 0, endLoopBusca #Se o nó for nulo
 	lw $t6, 0($a1)		 #Salvando endereco Tuardado na raiz	
 	lw $t0 , 0($t6) 	 #Carregando em Valor o valor do no raiz
 	la $t2, 0		 #zerando auxiliar
@@ -138,19 +139,19 @@ buscar: addi $sp, $sp, -4
 		addi $t1 , $zero, 0 #zerando ptr		
 		addi $v0, $t2, 0 	#Salvando o endereco do potencial pai em $v0 para retorno
 	endLoopBusca:
-		#final da funï¿½ï¿½o buscar
+		#final da função buscar
 		lw $ra, 0($sp)
 		addi $sp, $sp, 4
 		jr $ra
 		
-#$a1 O endereco de memï¿½ria da raiz
+#$a1 O endereco de memória da raiz
 #$a0 o valor que eu quero inserir
 #$t1 resultado da busca
 #$t2 auxiliar
 inserir:
 	addi $sp, $sp, -4
 	sw $ra, 0($sp)
-	#Com o endereco da raiz e o valor que eu quero inserir eu faï¿½o uma chamada de busca
+	#Com o endereco da raiz e o valor que eu quero inserir eu faço uma chamada de busca
 	addi $s0, $a0, 0 #salvando valor buscado no registrador
 	addi $s7, $a1, 0 #Salvando em #$s7 o endereco de memoria da raiz
 	jal buscar
@@ -159,7 +160,7 @@ inserir:
 	beq $t1, 0, gravarRaiz #Se for nulo, passa para gravar na raiz
 		addi $t2 , $zero, 0 #zerando $t2
 		lw $t7, 0($t1) #Careegando o valor em $t7
-		slt $t6, $s0, $t7#se o valor que eu  quero inserir ï¿½ menor
+		slt $t6, $s0, $t7#se o valor que eu  quero inserir é menor
 		bne $t6, 1, elseMenor
 		jal malloc
 		sw $v0, 24($t1) #Gravando o endereco alocado em malloc em $t1
@@ -190,8 +191,8 @@ inserir:
 	gravarRaiz:
 		addi $s1, $a1, 0 #salvando em $s1 o endereco guardado em  $s1
 		jal malloc
-		sw $v0, 0($a1) #salvando o endereco de memï¿½ria no espaco da memoria guardado para o node raiz
-		lw $t6, 0($a1) #carregando o endereco do node raiz da memï¿½ria
+		sw $v0, 0($a1) #salvando o endereco de memória no espaco da memoria guardado para o node raiz
+		lw $t6, 0($a1) #carregando o endereco do node raiz da memória
 		sw $s0, 0($t6) #Salvando o valor na raiz
 		sw $zero, 24($t6)  #Zerando Filho esquerda
 		sw $zero, 28($t6)  #zerando filho direita
@@ -205,17 +206,17 @@ inserir:
 		addi $sp, $sp, 4
 		jr $ra
 		
-#Performando rotaï¿½ï¿½es===============================================================================				
+#Performando rotações===============================================================================				
 #$a0 -> noARotacionar
 #$a1 -> NoArvore
 #$t0 -> ptr , 
-#$t1 -> fEsq, $t2 ->registrador auxiliar para verificaï¿½ï¿½o
+#$t1 -> fEsq, $t2 ->registrador auxiliar para verificação
 RSD: #Rotacao simples direita
 	addi $sp, $sp, -4
 	sw $ra, 0($sp)
 	addi $t0, $a0, 0 #salvando o endereco em registrador
 	lw $t1,24($t0)   #carregando endereco de filho esquerda $t0
-	lw $t2, 28($t1)  #carregando em registrdor auxiliar o endereï¿½o Fdir 
+	lw $t2, 28($t1)  #carregando em registrdor auxiliar o endereço Fdir 
 	sw $t2,24($t0)   #salvando o endereco carregado no endereco de Fesq
 	beq $t2, 0, seguirRSD
 		sw $t0,20($t2) #Setando pai de no rotacionado
@@ -243,7 +244,7 @@ RSD: #Rotacao simples direita
 
 	lw $t2, 20($t1) #carregando no pai de $t1 em registrador t2
 	beq $t2, 0, gravarNovaRaizRSD
-		#Serï¿½ utilixado um registradores $t3 e $t4
+		#Será utilixado um registradores $t3 e $t4
 		lw $t3, 0($t2) #carregando valor do node pai
 		lw $t4, 0($t1) #Carregando valor do node Fesq
 		slt $t7, $t4, $t3
@@ -267,13 +268,13 @@ endRSD:
 #$a0 -> noARotacionar
 #$a1 -> NoArvore
 #$t0 -> ptr , 
-#$t1 -> fDir, $t2 ->registrador auxiliar para verificaï¿½ï¿½o	
+#$t1 -> fDir, $t2 ->registrador auxiliar para verificação	
 RSE: #Rotacao simples esquerda
 	addi $sp, $sp, -4
 	sw $ra, 0($sp)
 	addi $t0, $a0, 0 #salvando o endereco em registrador
 	lw $t1,28($t0)   #carregando endereco de filho Direita $t0
-	lw $t2, 24($t1)  #carregando em registrdor auxiliar o endereï¿½o Fdir (Filo Esq do filho Dir)
+	lw $t2, 24($t1)  #carregando em registrdor auxiliar o endereço Fdir (Filo Esq do filho Dir)
 	sw $t2,28($t0)   #salvando o endereco carregado no endereco de Fesq
 	beq $t2, 0, seguirRSE
 		sw $t0,20($t2) #Setando pai de no rotacionado
@@ -303,7 +304,7 @@ RSE: #Rotacao simples esquerda
 					
 	lw $t2, 20($t1) #carregando no pai de $t1 em registrador t2
 	beq $t2, 0, gravarNovaRaizRSE
-		#Serï¿½ utilixado um registradores $t3 e $t4
+		#Será utilixado um registradores $t3 e $t4
 		lw $t3, 0($t2) #carregando valor do node pai
 		lw $t4, 0($t1) #Carregando valor do node FDir
 		slt $t7, $t4, $t3
@@ -323,7 +324,7 @@ endRSE:
 	addi $sp, $sp, 4
 	jr $ra	
 
-#verificando rotaï¿½ï¿½es=============================================================
+#verificando rotações=============================================================
 #$a0 -> endereco do node desbalanceado
 #$a1 -> Endereco do do node Raiz
 #$t0 ->Temporario para guardar fbDo no desbalanceado
@@ -331,7 +332,7 @@ endRSE:
 verificandoRotacoes:
 	addi $sp, $sp, -4
 	sw $ra, 0($sp)
-	beq $a0, 0, endVR # se o no nï¿½o for nulo
+	beq $a0, 0, endVR # se o no não for nulo
 	lw $t0, 16($a0) #carregando FB do no em $t0
 	bne $t0, 2, verDoisNeg
 		lw $t0, 28($a0) #carregando o endereco do filho a direita em $t0
@@ -340,8 +341,8 @@ verificandoRotacoes:
 			addi $t8, $t0, 0 #Salvando o endereco do Direita em $t8
 			lw $t0, 16($t8) #Fator de balanceamento do filhoDireita
 			seq $t7, $t0, -1
-			bne $t7,1,elseVRE#Verificar Rotaï¿½ï¿½o esquerda
-			#Rotaï¿½ï¿½o dupla esquerda
+			bne $t7,1,elseVRE#Verificar Rotação esquerda
+			#Rotação dupla esquerda
 			addi $s3, $a0, 0 #salvando endereco do node desbalanceado em $s3
 			lw $a0, 28($a0) #salvando em $a0 o endereco do filho a direita
 			jal RSD			
@@ -358,8 +359,8 @@ verificandoRotacoes:
 			addi $t8, $t0,0 #Salvando o endereco do Fesq em $t8
 			lw $t0, 16($t8) #Fator de balanceamento do filhoEsquerda
 			seq $t7, $t0, 1
-			bne $t7,1,elseVRD#Verificar Rotaï¿½ï¿½o esquerda
-			#Rotaï¿½ï¿½o dupla Direita
+			bne $t7,1,elseVRD#Verificar Rotação esquerda
+			#Rotação dupla Direita
 			addi $s3, $a0, 0 #salvando endereco do node desbalanceado em $s3
 			lw $a0, 24($a0) #salvando em $a0 o endereco do filho a esquerda
 			jal RSE			
@@ -373,7 +374,7 @@ endVR:
 	addi $sp, $sp, 4
 	 jr $ra
 
-#Funï¿½ï¿½es para calcular fatores de balanceamento===================================
+#Funções para calcular fatores de balanceamento===================================
 #$a0 -> endereco do no cujoFatorSeraCalculado
 #$t0 -> ptr, $t1(ponteiro auxiliar), $t2 hEsq , $t3Hdir altura, $t4 
 calcularFatorBalanceamento:
@@ -382,7 +383,7 @@ calcularFatorBalanceamento:
 	addi $t0, $a0, 0
 	lw $t1, 24($t0)#Carregando endereco filhoEsq
 	beq $t1, 0, elseEsqNull
-		lw $t2, 4($t1) #Pegando altura do filho ï¿½ esqueda
+		lw $t2, 4($t1) #Pegando altura do filho à esqueda
 		sw $t2, 8($t0) #Salvando altura do filho a esqueda em altura esquerda
 	j verEsq
 elseEsqNull:	
@@ -400,7 +401,7 @@ endCalcularFatorBal:
 	lw $t2, 8($t0)	#carregando altura mais a esquerda
 	lw $t3, 12($t0) #carregando altura mais a direita
 	sub $t4, $t3,$t2 #fazendo o calculo do FatBal
-	sw $t4, 16($t0) #salvando o FatBal no nï¿½
+	sw $t4, 16($t0) #salvando o FatBal no nó
 	#verificando maior
 	sge $t7, $t3, $t2
 	bne $t7, 1, max 
@@ -418,13 +419,13 @@ endCalcularFatorBal:
 
 #Atualizar fator balanceamento=============================================================
 #$a0 -> O node inserido
-#$a1 -> o endereco de memoria da ï¿½rvore
+#$a1 -> o endereco de memoria da árvore
 #$t0 -> ptr; $t1, $t2 Registradores auxiliares
 atualizarFatorBalanceamento:
 	addi $sp, $sp, -4
 	sw $ra, 0($sp)
-	addi $t0, $a0, 0#carregando o endereco de memï¿½ria do node raiz
-#O seguinte loop atualiza o fator de balanceamento do nodes e performa rotaï¿½ï¿½es
+	addi $t0, $a0, 0#carregando o endereco de memória do node raiz
+#O seguinte loop atualiza o fator de balanceamento do nodes e performa rotações
 #nos nodes desbalanceados
 loopAtualizar:
 	beq $t0, 0, endLoopAtualizar
@@ -453,19 +454,19 @@ malloc:
 	addi $t9, $t9, 32
 	sw $t9, valAlocation #Salvando o endereco em alocation
 	addi $t9, $zero, 0   #zerando o $t9	
-	#O endereco dinamicamente alocado estï¿½ em $v0
+	#O endereco dinamicamente alocado está em $v0
 	lw $ra, 0($sp)		
 	addi $sp, $sp, 4
-	jr $ra	#retorna com o endereco de alocaï¿½ï¿½o em $v0
+	jr $ra	#retorna com o endereco de alocação em $v0
 
 #a1 ->endereco do node raiz
-#$t0 -> registrador auxiliar para guardar endereco de memï¿½ria
+#$t0 -> registrador auxiliar para guardar endereco de memória
 posOrdem:
 	addi $sp, $sp, -8 #Dis espacos um para o endereco de retorno e outro para o endereco do node
 	sw $ra, 0($sp)
-	sw $a1, 4($sp) 	#Gravando endereco de  memï¿½ria na pilha
+	sw $a1, 4($sp) 	#Gravando endereco de  memória na pilha
 	lw $t0, 28($a1) #pegando o endereco do filho a direita
-	beq $t0, 0, poFEsq # se o filho mais a direita for nulo dï¿½ um branch para verificar o filho mais a esquerda
+	beq $t0, 0, poFEsq # se o filho mais a direita for nulo dá um branch para verificar o filho mais a esquerda
 		addi $a1, $t0,0 #salva o endereco guardado em $t0 em $a0
 		jal posOrdem	#faz uma chamada recursiva 
 poFEsq:	  
@@ -485,11 +486,44 @@ imprimirValor:
 	addi $sp, $sp, 8
 	jr $ra
 
+#a1 ->endereco do node raiz
+#$t0 -> registrador auxiliar para guardar endereco de memória
+buscaOrdem:
+	addi $sp, $sp,-8 #Decrementando o stack pointer
+	sw $ra,0($sp)    #Salvando o return address na pilha
+	sw $a1,4($sp)    #salvando o endereco do node pilha
+	addi $t0,$a1,0 #carregando o endereco do node raiz
+	lw $t1, 24($a1) #carregando valo do filho mais a esquerda
+	beq $t1, 0,saiFilhoEsq #Se o filho mais a direita for nulo vá praa o node pai
+		move $a1,$t1   #passando o endereco do filho mais a esquerda para $a1
+		jal buscaOrdem
+	saiFilhoEsq:
+		
+		lw $t0,4($sp)#carregando o endereco do node no registrador t0
+		lw $a2,0($t0)#Carregando o valor salvo no node
+		jal escreverValor
+	lw $t0,4($sp)	#carregando o endereco do node no registrador $t0
+	lw $t1,0($t0)   #carregand o valor salvo no node em $t1
+
+	beq $t1,0, saiFilhoDir #Se o filho da direita for nulo 
+		move $a1,$t1 #passando o endereco do filho mais a esquerda para $a1
+	saiFilhoDir:
+		
+	lw $t0,4($sp)	#Carregando o endereco do node salvo na pilha em $t0 novamente por garantia
+	lw $t1,28($t0)  #Carregando o valor do endereco de memória do filho mais a esquerda
+	beq $t1,0,filhoDirNulo #Se o filho mais a direita for nulo 
+		addi $a1,$t1,0 #Senão passa o endereço do filho mais a direita
+		jal buscaOrdem
+	filhoDirNulo:		
+	
+	lw $ra,0($sp)
+	addi $sp,$sp,8
+	jr $ra
 
 #$a2 -> Guardara o valor a ser convertido
-#$a1 -> O endereco do buffer o qual serï¿½ usado para armazenar a cadeia
+#$a1 -> O endereco do buffer o qual será usado para armazenar a cadeia
 #$t3 -> rtegistrador contador do tamanho da string
-#$t0, $t1, $t2 -> registradore auxiliares para aconversï¿½o
+#$t0, $t1, $t2 -> registradore auxiliares para aconversão
 escreverValor:	 
 	addi $sp, $sp, -4
 	sw $ra, 0($sp) 
@@ -499,17 +533,17 @@ escreverValor:
 	addi $t0, $a2, 0#Salvando dividento
 	li  $t1, 10	#carregando divisor
 	li $t3, 0	#Carregando Contador de digito
-	addi $a1, $a1, 3 #Comeï¿½ando no final do buffer da strings
+	addi $a1, $a1, 4 #Começando no final do buffer da strings
 	sb $zero, 0($a1) #Gravando bite nulo
 convIntChar:
-	divu $t0, $t1 	#Dividindo o valor pelo divisor, pois ï¿½ a primeiraoperacao
+	divu $t0, $t1 	#Dividindo o valor pelo divisor, pois é a primeiraoperacao
 	mflo $t0	#Pegando o quiciente
 	mfhi $t2	#PEgando o resto
 	addiu $t2, $t2, 0x30 #convertendo o resto em caractere
 	addiu $a1, $a1, -1
 	sb $t2, 0($a1)     #Gravar o digito convertido
 	addi $t3, $t3, 1  #Icrementando Contador de digito
-	bne $t0, 0,convIntChar #prï¿½xima iteracao
+	bne $t0, 0,convIntChar #próxima iteracao
 endConv:	
 					
 	li $v0, 15
